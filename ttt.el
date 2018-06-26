7a505777e7e0e2b60285ba2a50ffcc5ea667bc55
KrokodileGlue <KrokodileGlue@outlook.com>
2018-06-26 15:56:12 -0700

    Modify package metadata

--- ttt.el
+++ ttt.el
@@ -1,10 +1,35 @@
-;;; ttt.el --- Tic-Tac-Toe in Emacs Lisp.
+;;; ttt.el --- Tic-Tac-Toe game
+
+;; Author: KrokodileGlue <KrokodileGlue@outlook.com>
+;; Keywords: game games two-player inane diversion
+;; Version: 0.1beta
+;; URL: https://github.com/KrokodileGlue/ttt
+
+;; This file is not part of GNU Emacs.
+
+;; Permission is hereby granted, free of charge, to any person
+;; obtaining a copy of this software and associated documentation
+;; files (the "Software"), to deal in the Software without
+;; restriction, including without limitation the rights to use, copy,
+;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;; of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+;; BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+;; ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+;; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
 
 ;;; Commentary:
-;;; Start a game of Tic-Tac-Toe with `M-x ttt`.
-
-;;; Version: 0.1beta
-;;; URL: https://github.com/KrokodileGlue/ttt
+;; This package provides a command and major mode for playing
+;; two-player games of tic-tac-toe.
 
 ;;; Code:
 
